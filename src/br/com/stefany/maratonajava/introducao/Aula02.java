133a324349ba0191b004cafa16d802e6d8006756
stefany <stefany.silveira21@gmail.com>
2023-01-05 18:28:43 -0300

    Aula 02 - Tipos primitivos

---
+++ src/br/com/stefany/maratonajava/introducao/Aula02.java
@@ -0,0 +1,9 @@
+package br.com.stefany.maratonajava.introducao;
+
+public class Aula02 {
+    public static void main(String[] args) {
+
+        int idade = 10;
+        System.out.println("A idade é: " + idade + " anos");
+    }
+}
