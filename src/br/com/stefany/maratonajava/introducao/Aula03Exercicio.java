a0908a35ab60ad8b6ab0c9f9b12727da866664f0
stefany <stefany.silveira21@gmail.com>
2023-01-27 13:48:13 -0300

    Aula 02 pt 5 - Exercício

---
+++ src/br/com/stefany/maratonajava/introducao/Aula03Exercicio.java
@@ -0,0 +1,22 @@
+package br.com.stefany.maratonajava.introducao;
+
+/* Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+   Eu <nome>, morando no endereço <endereço>,
+   confirmo que recebi o salário de <salario>, na data <data>
+    */
+public class Aula03Exercicio {
+
+    public static void main(String[] args) {
+
+        String nome = "Stefany";
+        String endereco = "Avenida São Paulo 20, Jardim Brasil, SP - CEP 01234-567";
+        String dataRecebimentoSalario = "27/01/2023";
+        double salario = 2000;
+
+        System.out.println("Eu " + nome  + ", morando no endereço " + endereco
+                + ", confirmo que recebi o salário de " + salario  + ", na dataRecebimentoSalario " + dataRecebimentoSalario);
+
+
+    }
+}
