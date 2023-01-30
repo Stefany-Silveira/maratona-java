45d40453547921f94a63105f1bf50d1d2c1d415a
stefany <stefany.silveira21@gmail.com>
2023-01-30 15:48:58 -0300

    Aula 05 pt 1 - Estruturas Condicionais IF

---
+++ src/br/com/stefany/maratonajava/introducao/Aula05.java
@@ -0,0 +1,21 @@
+package br.com.stefany.maratonajava.introducao;
+
+// Estruturas condicionais
+public class Aula05 {
+
+    public static void main(String[] args) {
+
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcoólica");
+        }
+
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Não autorizado a comprar bebida alcoólica");
+        }
+
+        System.out.println("Fora do if");
+    }
+}
