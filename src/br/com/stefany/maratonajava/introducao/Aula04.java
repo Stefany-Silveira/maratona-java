cce46ecb79f38b8632c1adcdfb3898d84ce5d554
stefany <stefany.silveira21@gmail.com>
2023-01-30 15:16:00 -0300

    Aula 04 pt 5- Operadores de Atribuição

--- src/br/com/stefany/maratonajava/introducao/Aula04.java
+++ src/br/com/stefany/maratonajava/introducao/Aula04.java
@@ -21,6 +21,9 @@ public class Aula04 {
         int resto = 21 % 7;
         System.out.println(resto);
 
+        System.out.println("---------------------------------------------");
+        System.out.println("---------------------------------------------");
+
         // <  > <= >= == !=
         boolean isDezMaiorQueVinte = 10 > 20;
         boolean isDezMenorQueVinte = 10 < 20;
@@ -34,6 +37,9 @@ public class Aula04 {
         System.out.println("isDezIgualDez " + isDezIgualDez);
         System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
 
+        System.out.println("---------------------------------------------");
+        System.out.println("---------------------------------------------");
+
         // && (AND) || (or)
         int idade = 29;
         float salario = 3500f;
@@ -43,6 +49,9 @@ public class Aula04 {
         System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
         System.out.println("isDentroDaLeiMneorQueTrinta " + isDentroDaLeiMenorQueTrinta );
 
+        System.out.println("---------------------------------------------");
+        System.out.println("---------------------------------------------");
+
         // || OR
         double valorTotalContaCorrente = 200;
         double valorTotalContaPoupanca = 10000;
@@ -50,6 +59,41 @@ public class Aula04 {
 
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
+
+        System.out.println("---------------------------------------------");
+        System.out.println("---------------------------------------------");
+
+        //  = += -= *= /= %=
+        double bonus = 1800; //1800
+        System.out.println("Bônus: " + bonus);
+
+        bonus += 1000; // 2800
+        System.out.println("Bônus: 1800 + 1000 = " + bonus);
+
+        bonus -= 1000; // 1800
+        System.out.println("Bônus: 2800 - 1000 = " + bonus);
+
+        bonus *= 2; // 3600
+        System.out.println("Bônus: 1800 * 2 = " + bonus);
+
+        bonus /= 2; // 1800
+        System.out.println("Bônus: 3600 / 2 = " + bonus);
+
+        bonus %= 2;
+        System.out.println("Bônus: 1800 % 2 = " + bonus);
+
+        System.out.println(bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+
+        int contador2 = 0;
+        System.out.println(++contador2);
 
     }
 }
