366b9643335955e50603059693977d5f724a43bf
stefany <stefany.silveira21@gmail.com>
2023-02-01 15:47:39 -0300

    Aula 07 pt 1 - Arrays

---
+++ src/br/com/stefany/maratonajava/introducao/Aula07.java
@@ -0,0 +1,16 @@
+package br.com.stefany.maratonajava.introducao;
+
+//Arrays
+public class Aula07 {
+    public static void main(String[] args) {
+
+        int [] idades = new int[3];
+        idades[0] = 20;
+        idades[1] = 35;
+        idades[2] = 56;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
