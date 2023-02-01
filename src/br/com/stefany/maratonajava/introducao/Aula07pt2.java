79854b6852055c542da5add445fc93ea70379cf4
stefany <stefany.silveira21@gmail.com>
2023-02-01 15:56:49 -0300

    Aula 07 pt 2 - Arrays

---
+++ src/br/com/stefany/maratonajava/introducao/Aula07pt2.java
@@ -0,0 +1,18 @@
+package br.com.stefany.maratonajava.introducao;
+
+//Arrays
+public class Aula07pt2 {
+    //byte, short, int, long, float e double 0
+    //char '\u0000' ' '
+    // booleamn false
+    //String null
+
+    public static void main(String[] args) {
+
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+
+    }
+}
